59bd2cac3434ddb5ee6ab06b5720489438354e25
mvitoriazx <mvitoriasantos017@gmail.com>
2026-08-12 21:26:42 -0300

    Add files via upload

---
+++ src/flamingo/apredendo/introducao/Atalhos.java
@@ -0,0 +1,11 @@
+package flamingo.apredendo.introducao;
+
+public class Atalhos {
+    //PSVM
+    // sout
+    public static void main(String[] args) {
+        String nome = "Vitoria";
+        System.out.println("Meu nome é" + nome);
+
+    }
+}
---
+++ src/flamingo/apredendo/introducao/ExercicioPraticaBasica.java
@@ -0,0 +1,11 @@
+package flamingo.apredendo.introducao;
+
+public class ExercicioPraticaBasica {
+    /*
+    Eu <nome> morando no endereço <endereco>, confirmo o salário
+    de <salario> na data <data>
+     * */
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
